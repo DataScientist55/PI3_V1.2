6c1d40bbd81698c70769b63f72931d5dffa7d54d
DataScientist55 <2214802@aluno.univesp.br>
2025-04-29 23:14:29 -0300

    Adiciona dotenv ao projeto e configura settings.py

--- EstEsc/settings.py
+++ EstEsc/settings.py
@@ -3,8 +3,11 @@ import os
 
 from pathlib import Path
 
-from dotenv import load_dotenv
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
