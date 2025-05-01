a091eb4b386241a339ec655d514b45d0c25bd7cc
brunofigueh <bruno_alqui@hotmail.com>
2025-04-30 22:23:35 -0300

    home migrations chance

--- EstEsc/settings.py
+++ EstEsc/settings.py
@@ -92,12 +92,9 @@ if DJANGO_ENV == 'production':
 else:  # Desenvolvimento local
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'vecchio',
-        'PASSWORD': 'V3cc#!o55#',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+      
     }
 }
 
