c0b37cdd869d71c79a6489e6f1b61a952be59fc4
brunofigueh <bruno_alqui@hotmail.com>
2025-05-03 15:26:13 -0300

    fix erros sintaxa template confirmar_exclusao_usuario

--- home/forms.py
+++ home/forms.py
@@ -21,7 +21,7 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username","first_name", "last_name", "email", "password1", "password2")
 
 class RequisicaoForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
